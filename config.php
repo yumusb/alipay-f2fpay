b52ef5fef17bba3193fa51769cf10ca39791231a
榆木 <43062104+yumusb@users.noreply.github.com>
2020-05-06 13:26:23 +0800

    修复建表的一个小bug
    当初建表不够严谨。mysql5.7（好像？）中会有问题。现在建表时候给定验证时间、支付宝订单号、付款账号 一个默认空值。

--- config.php
+++ config.php
@@ -53,27 +53,32 @@ $alipay_config = array(
 if ($alipay_config['alipay_public_key'] == '' || $alipay_config['merchant_private_key'] == '' || $alipay_config['app_id'] == '') {
 	exit("alipay_public_key/merchant_private_key/app_id must not be null");
 }
-/* 创建订单表。直接复制以下内容，然后选中数据库 执行就可成功创建
+/* 创建订单表。直接复制以下内容，然后选中数据库 执行就可成功创建 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 CREATE TABLE `f2f_order` (
   `id` varchar(50) NOT NULL COMMENT '订单号',
-  `mark` VARCHAR(50) NOT NULL COMMENT '备注',
+  `mark` varchar(50) NOT NULL COMMENT '备注',
   `mount` varchar(20) NOT NULL COMMENT '订单金额',
-  `notify_time` varchar(20) NOT NULL COMMENT '订单验证时间',
-  `trade_no` varchar(30) NOT NULL COMMENT '支付宝订单号',
-  `buyer_logon_id` varchar(30) NOT NULL COMMENT '付款账号',
+  `notify_time` varchar(20) DEFAULT NULL COMMENT '订单验证时间',
+  `trade_no` varchar(30) DEFAULT NULL COMMENT '支付宝订单号',
+  `buyer_logon_id` varchar(30) DEFAULT NULL COMMENT '付款账号',
   `status` varchar(10) NOT NULL COMMENT '订单状态'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO `f2f_order` (`id`, `mark`, `mount`, `notify_time`, `trade_no`, `buyer_logon_id`, `status`) VALUES
+('933a092fa70f488af8fc5e559ba840f6', '好好学习', '0.01', NULL, NULL, NULL, 'nopay');
 
 
 ALTER TABLE `f2f_order`
   ADD PRIMARY KEY (`id`);
 COMMIT;
+
+
 */
 // $database=array(
 // 	'dbname'=>'f2f',
